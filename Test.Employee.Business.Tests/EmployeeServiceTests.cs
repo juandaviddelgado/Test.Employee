00e2d83055b6b10e616d5d9b412d8ad46eada74f
Juan David Delgado Ramirez <juandelgado.ing@gmail.com>
2019-07-12 00:50:02 -0500

    Unit test working

---
+++ Test.Employee.Business.Tests/EmployeeServiceTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using Test.Employee.Business.Interfaces;
+using Test.Employee.DataAccess.Interfaces;
+
+namespace Test.Employee.Business.Tests
+{
+    [TestClass]
+    public class EmployeeServiceTests
+    {
+        private Mock<IEmployeeRepository> mockEmployeeRepository;
+        private IEmployeeService employeeService;
+        private IEnumerable<Entities.Employee> employeeListMockRepository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.employeeListMockRepository = new Entities.Employee[] {
+                new Entities.Employee()
+                {
+                    Id= 1,
+                    Name="Juan",
+                    ContractTypeName= "MonthlySalaryEmployee",
+                    HourlySalary = 30,
+                    MonthlySalary = 120
+                },
+                new Entities.Employee()
+                {
+                    Id= 2,
+                    Name="Pedro",
+                    ContractTypeName= "HourlySalaryEmployee",
+                    HourlySalary = 17,
+                    MonthlySalary = 80
+                }
+            };
+            mockEmployeeRepository = new Mock<IEmployeeRepository>();
+            mockEmployeeRepository.Setup(repository => repository.Get()).ReturnsAsync(this.employeeListMockRepository);
+
+            employeeService = new EmployeeService(mockEmployeeRepository.Object);
+        }
+
+        [TestMethod]
+        public void GetAllRecordsNotEspecificId()
+        {
+            var employeeList = this.employeeService.Get().GetAwaiter().GetResult();
+            Assert.AreEqual(employeeList.Count(), this.employeeListMockRepository.Count());
+        }
+
+        [TestMethod]
+        public void GetRecordByEspecificId()
+        {
+            var employeeList = this.employeeService.Get(1).GetAwaiter().GetResult();
+            Assert.AreEqual(employeeList.Count(), 1);
+            Assert.AreEqual(employeeList.FirstOrDefault().Id, 1);
+        }
+
+        [TestMethod]
+        public void GetRightAnualSalaryBaseOnMonthlyContract()
+        {
+            var employeeList = this.employeeService.Get(1).GetAwaiter().GetResult();
+
+            Assert.AreEqual(employeeList.FirstOrDefault().AnualSalary, employeeList.FirstOrDefault().MonthlySalary * 12);
+        }
+
+        [TestMethod]
+        public void GetRightAnualSalaryBaseOnHourlyContract()
+        {
+            var employeeList = this.employeeService.Get(2).GetAwaiter().GetResult();
+
+            Assert.AreEqual(employeeList.FirstOrDefault().AnualSalary, employeeList.FirstOrDefault().HourlySalary * 12 * 120);
+        }
+    }
+}
---
+++ Test.Employee.Business.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test.Employee.Business.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test.Employee.Business.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6c7d144d-ff3d-4c96-b5a8-9f8fd7e90bbe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
